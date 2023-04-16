2eb9ac22c360642ce1116668acfef33664646579
Rjchauhan18 <rjchauhan5000000@gmail.com>
2023-04-16 12:49:29 +0530

    CHANGE TO SMTP EMAIL SENDING

--- main.py
+++ main.py
@@ -2,40 +2,18 @@ import os
 from bs4 import BeautifulSoup 
 import requests
 import time 
-import pywhatkit
-import pyautogui
-from pynput.keyboard import Key, Controller
-from dotenv import load_dotenv
+import smtplib
 
-load_dotenv(".env")
+# smtp connection
 
-Number = os.environ.get["NUMBER"]
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+sender_email = os.environ.get['SMTP_SENDER_EMAIL']
+smtp_password =  os.environ.get['SMTP_PASSWORD']
+receiver_email =  os.environ.get['SMTP_RECEIVER_EMAIL']
 
-keyboard = Controller()
-
-
-os.environ['DISPLAY'] = ':0'
-
-
-def send_whatsapp_message(number:int,msg: str):
-    try:
-        pywhatkit.sendwhatmsg_instantly(
-            phone_no=f"+91{number}", 
-            message=msg,
-            tab_close=True
-        )
-        time.sleep(5)
-        pyautogui.click()
-        time.sleep(2)
-        keyboard.press(Key.enter)
-        keyboard.release(Key.enter)
-
-        print("Message sent!\n\n" + msg)
-    except Exception as e:
-        print(str(e))
-
-# <a href="#" id="ContentPlaceHolder1_lvCircular_lblContentHeading_0"></a>
-
+server.login(sender_email,smtp_password)
+print("successful connected")
 
 r = requests.get("https://www.gtu.ac.in/Circular.aspx")
 
@@ -45,29 +23,11 @@ except Exception as e:
     print(e)
 
 h3_tag=html.find("h3",{"class":"d-block"})
-# print(element_by_class.prettify())
-
-# try:
-#     file = open("circular.html", "w",encoding="utf-8")
-#     file.write(element_by_class.prettify())
-# except Exception as e:
-#     print(e)
-
-
-# top_circular = h3_tag.find("a" , {"class":"most-popular-post"})
-# print(top_circular.pret
-# tify())
 
 # title and link of the content
 
 with open("save.txt", "r") as f:
     last_notification = f.readlines()
-    # print(last_n/otification[0])
-
-
-
-# with open("save.txt", "w") as f:
-#             f.writelines("link"+" rahul chauhan")
 
 
 def info():
@@ -77,27 +37,20 @@ def info():
     link_tag=h3_tag.find("a",{"target" :"_blank"},href=True)
     link= link_tag.get('href')
 
-    # print(dt)
-    # print(link_tag.text)
-
     if last_notification[0] != link:
         try:
-            msg = (dt + "\n"+link_tag.text+ "\n"+link + "\n")
+            msg = (dt + "\n\n"+link_tag.text+ "\n\n"+link + "\n")
+            server.sendmail(sender_email,receiver_email,msg)
         except Exception as e:
             print(e)
 
-        # print(os.getenv('number'))
-        
-        send_whatsapp_message(Number,msg)
         with open("save.txt", "w") as f:
             f.writelines(link)
-            # last_link.append(link)
     else:
         print("No latest notification")
 
 
 
 if __name__ == "__main__":
-# while True:
     info()
 
