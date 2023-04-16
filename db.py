22274d0cf73de30331dd4e2fe5401de2d67ae60a
Rjchauhan18 <113838908+Rjchauhan18@users.noreply.github.com>
2023-04-16 14:24:59 +0530

    Add files via upload

---
+++ db.py
@@ -0,0 +1,34 @@
+import mysql.connector
+
+
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user='root',
+  password='Rjchauhan@18',
+  database="bank"
+)
+
+print(mydb)
+mycursor = mydb.cursor(dictionary=True)
+print("connection done")
+
+
+
+# mycursor.execute("CREATE TABLE GTU_NOTOFICATION (id INT AUTO_INCREMENT PRIMARY KEY,Date DATE,notification VARCHAR(255), link VARCHAR(255))")
+def insert_notification(notification,link):
+
+    sql = "INSERT INTO GTU_NOTOFICATION (notification, link) VALUES ( %s,%s)"
+    val = (notification,link)
+    mycursor.execute(sql, val)
+    mydb.commit()
+# insert_notification('notification3','link3')
+
+def last_notification():
+    mycursor.execute("SELECT * FROM GTU_NOTOFICATION ORDER BY id desc limit 1")
+
+    myresult = mycursor.fetchone()
+    myresult['link']
+    return myresult['link']
+
+
