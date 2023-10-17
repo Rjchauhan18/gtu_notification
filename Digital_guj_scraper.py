2b1c322a1f7053a6db55eb744c882a4c0772b365
Braxton Elmer <raxtray@gmail.com>
2023-10-17 21:14:07 +0530

    Web Scraping

--- Digital_guj_scraper.py
+++ Digital_guj_scraper.py
@@ -1,86 +1,44 @@
-import os
-import requests 
+import requests
 from bs4 import BeautifulSoup
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+
+url = 'https://www.digitalgujarat.gov.in/loginapp/CitizenLogin.aspx'
+response = requests.get(url)
 
 
-def information():
-    r= requests.get("https://www.digitalgujarat.gov.in/loginapp/CitizenLogin.aspx")
-    html = BeautifulSoup(r.text,'html.parser')
+soup = BeautifulSoup(response.content, 'html.parser')
 
+tr_tags = soup.find_all('tr')
 
-    table = html.select('table')[0] 
-    TR=table.find_all('tr')
-    
-    with open("datafile.txt","r") as f:
-        last_data=f.read()
-    last_data_list=last_data.split('\n')
-    last_data=[x for x in last_data_list if x!=""]
-    print(last_data[0])
-    print(TR[0].text)
-    if last_data[0]==TR[0].text:
-        print(True)
-    else:
-        print(False)
-    
-    if last_data[0]==TR[0].text or last_data[1]==TR[1].text:
-        print("no change")
-    else:
-        with open("datafile.txt","w") as f:
-           
-                # print(i.text)
-            f.write(TR[0].text)
-
-            f.write("\n")
-
-            f.write(TR[1].text)
-            f.write("\n")
-            
-            print("added")
-                    
-    
-
-
-def Sendemail(date,receivers_email):
-  # Define email addresses to use
-    sender_email =os.environ.get('SMTP_SENDER_EMAIL')#sender email
-    smtp_pass = os.environ.get('SMTP_PASSWORD')# app generated password
-    # print(sender_email)
-    # print(smtp_pass)
-    # print(receivers_email)
-    # receivers_email =os.environ.get('SMTP_RECEIVER_EMAIL')# receiver email
-
-
-    # Define SMTP email server details
-    smtp_server = 'smtp.gmail.com'
-
-    # Construct email
-    msg = MIMEMultipart('alternative')
-    msg['To'] =receivers_email
-    msg['From'] = sender_email
-    msg['Subject'] = 'Latest GTU Notification'
-
-    # Create the body of the message (a plain-text and an HTML version).
-    # html = (date + '\n'+Notification+ '\n'+link)
-    html = (date )
-
-
-
-
-    part1 = MIMEText(html, 'html')
-
-    msg.attach(part1)
-
-    # Send the message via an SMTP server
-    s = smtplib.SMTP(smtp_server,587)
-    s.ehlo()
-    s.starttls()
-    s.login(sender_email,smtp_pass)
-    print("successful connected")
-    s.sendmail(sender_email, receivers_email, msg.as_string())
-    s.quit()
-
-if __name__ == '__main__':
-    information()
+# Loop through each <tr> tag and extract the text and links from the specified structure
+for tr_tag in tr_tags:
+    # Find all <td> tags inside the current <tr> tag
+    td_tags = tr_tag.find_all('td')
+    # Check if there are at least 2 <td> tags
+    if len(td_tags) >= 2:
+        # Find the <span> tag inside the 2nd <td> tag
+        span_tag = td_tags[1].find('span')
+        # Check if <span> tag is found
+        if span_tag:
+            # Find all <a> tags inside the <span> tag
+            a_tags = span_tag.find_all('a')
+            # Check if there are at least 2 <a> tags
+            if len(a_tags) >= 2:
+                # Extract text from the 2nd <a> tag
+                text = a_tags[1].text.strip()
+                # Get the href attribute if it exists, otherwise set it to an empty string
+                href = a_tags[1].get('href', '').strip()
+                # Initialize an empty string to store the formatted text and links
+                formatted_text = f"~^\\{text}#~!~#{url + href}~^\\"
+                # Loop through subsequent <a> tags and concatenate their text and links in the desired format
+                for a_tag in a_tags[2:]:
+                    link_text = a_tag.text.strip()
+                    # Get the href attribute if it exists, otherwise set it to an empty string
+                    link_href = a_tag.get('href', '').strip()
+                    if link_href:
+                        # Format the hyperlink in the desired format
+                        formatted_link = f"~^\\{link_text}#~!~#{url + link_href}~^\\"
+                    else:
+                        formatted_link = f"~^\\{link_text}#~!~#~^\\"
+                    # Concatenate formatted text and links
+                    formatted_text += formatted_link
+                print(formatted_text)
